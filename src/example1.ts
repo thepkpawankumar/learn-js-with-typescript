2fad9f430dd8e73257cdf8d5e957d63ef2a12e66
Pawan Kumar <87902584+thepkpawankumar@users.noreply.github.com>
2024-07-11 00:18:30 +0530

    Update example1.ts

--- src/example1.ts
+++ src/example1.ts
@@ -3,6 +3,7 @@ const strArr = ["One", "Two", "Three"];
 
 const getFirstArrayElement = <T>(arr: T[]): T => arr[0];
 
+
 console.log(getFirstArrayElement(nArr));
 
 console.log(getFirstArrayElement(strArr));
